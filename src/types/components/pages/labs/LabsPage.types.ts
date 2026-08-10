520e5e0d8cd3407ebad1a77799b6375f094f843f
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-10 23:56:51 +0700

    align labs page data with cms page

--- src/types/components/pages/labs/LabsPage.types.ts
+++ src/types/components/pages/labs/LabsPage.types.ts
@@ -6,10 +6,6 @@ import type {
 export interface LabsPageData {
 	page: PageEntryData;
 	context: PageBuilderContext;
-	metadata: {
-		title: string;
-		description: string;
-	};
 }
 
 export interface LabsPageProps {
