e6eaefc7b99d42586f1eacff3b372645fd0f59f6
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-10 23:01:10 +0700

    type policy page from CMS page entry

--- src/types/components/pages/policy/PolicyPage.types.ts
+++ src/types/components/pages/policy/PolicyPage.types.ts
@@ -1,11 +1,7 @@
-import type { PageBuilderConfig } from "@/types/components/pages/builder/PageBuilder.types";
+import type { PageEntryData } from "@/types/components/pages/builder/PageBuilder.types";
 
 export interface PolicyPageData {
-	metadata: {
-		title: string;
-		description: string;
-	};
-	builder: PageBuilderConfig;
+	page: PageEntryData;
 }
 
 export interface PolicyPageProps {
