e21f990fa77160e4522827ba9046e5301debda75
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-11 00:33:34 +0700

    simplify work detail page type

--- src/types/components/pages/work-detail/WorkDetailPage.types.ts
+++ src/types/components/pages/work-detail/WorkDetailPage.types.ts
@@ -1,28 +1,9 @@
-import type { PPageHeaderSliderAsideData } from "@/types/components/object/project/page-header/PPageHeader.types";
-import type { PArticleBlock } from "@/types/components/object/project/article/PArticle.types";
-import type { PPostNavigationItem } from "@/types/components/object/project/post-navigation/PPostNavigation.types";
-import type { PReviewsProps } from "@/types/components/object/project/reviews/PReviews.types";
-import type { PageBuilderConfig } from "@/types/components/pages/builder/PageBuilder.types";
-
-export interface WorkDetailPageSource {
-	pageHeader: PPageHeaderSliderAsideData;
-	labels: {
-		onThisPage: string;
-		previous: string;
-		next: string;
-	};
-	tableOfContents: Array<{ label: string; href: string }>;
-	articleBlocks: PArticleBlock[];
-	reviews: PReviewsProps;
-	navigation: PPostNavigationItem[];
-}
+import type { Project } from "@/types/content";
 
 export interface WorkDetailPageData {
-	project: {
-		title: string;
-		summary: string;
-	};
-	builder: PageBuilderConfig;
+	project: Project;
+	previous?: Project;
+	next?: Project;
 }
 
 export interface WorkDetailPageProps {
