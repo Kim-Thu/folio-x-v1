05097be9fd80cbad86439deb019a91fea517d451
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-10 22:43:54 +0700

    load homepage directly from content

--- src/data/pages/getHomePageData.ts
+++ src/data/pages/getHomePageData.ts
@@ -1,8 +1,6 @@
 import { getPage } from "@/data/cms";
-import { resolvePage } from "@/data/pages/resolvePage";
 import type { HomePageData } from "@/types/components/pages/home/HomePage.types";
 
 export async function getHomePageData(): Promise<HomePageData> {
-	const page = await getPage("/");
-	return resolvePage(page);
+	return getPage("/");
 }
