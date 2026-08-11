c55a371880a64e1d467a02224fc4029f53c196d6
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-12 02:27:20 +0700

    type lab detail CMS presentation settings

--- src/types/components/pages/lab-detail/LabDetailPage.types.ts
+++ src/types/components/pages/lab-detail/LabDetailPage.types.ts
@@ -1,8 +1,10 @@
 import type { Lab } from "@/types/content";
+import type { CollectionEntry } from "astro:content";
 
 export interface LabDetailPageData {
 	lab: Lab;
 	relatedLabs: Lab[];
+	presentation: CollectionEntry<"labDetailSettings">["data"];
 }
 
 export interface LabDetailPageProps {
