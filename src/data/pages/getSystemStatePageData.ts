5ab125cd6051303e0a5da1d629636601b754a7fa
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-12 00:37:56 +0700

    simplify system state page data

--- src/data/pages/getSystemStatePageData.ts
+++ src/data/pages/getSystemStatePageData.ts
@@ -4,25 +4,5 @@ import type { SystemStateContent } from "@/types/content/SystemStateContent";
 export function getSystemStatePageData(
 	content: SystemStateContent,
 ): SystemStatePageData {
-	return {
-		builder: {
-			layout: {
-				template: "centered",
-			},
-			regions: [
-				{
-					key: "status",
-					component: "status",
-					section: {
-						id: content.id,
-						container: "site",
-					},
-					props: {
-						template: "split-media",
-						data: content,
-					},
-				},
-			],
-		},
-	};
+	return { content };
 }
