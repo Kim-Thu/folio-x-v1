f693c7d22f50a09ba19b533cdefb6a0be0a7285c
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-12 23:24:45 +0700

    expose blog detail presentation settings

--- src/data/cms.ts
+++ src/data/cms.ts
@@ -22,6 +22,7 @@ export const getInterfaceSettings = () => getSingleton("interfaceSettings");
 export const getFooterSettings = () => getSingleton("footerSettings");
 export const getSystemStatesSettings = () => getSingleton("systemStatesSettings");
 export const getLabDetailSettings = () => getSingleton("labDetailSettings");
+export const getBlogDetailSettings = () => getSingleton("blogDetailSettings");
 export const getPublicationDetailSettings = () => getSingleton("publicationDetailSettings");
 
 export async function getPage(slug: string): Promise<CollectionEntry<"pages">["data"]> {
