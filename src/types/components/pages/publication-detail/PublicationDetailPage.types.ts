3454c7c828b1427caf0fb4928165992351381428
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-12 23:04:23 +0700

    type publication detail presentation settings

--- src/types/components/pages/publication-detail/PublicationDetailPage.types.ts
+++ src/types/components/pages/publication-detail/PublicationDetailPage.types.ts
@@ -1,10 +1,12 @@
 import type { PublicationEntry } from "@/types/content";
+import type { CollectionEntry } from "astro:content";
 
 export type PublicationCollection = "comics" | "novels";
 
 export interface PublicationDetailPageData {
 	entry: PublicationEntry;
 	collection: PublicationCollection;
+	presentation: CollectionEntry<"publicationDetailSettings">["data"];
 }
 
 export interface PublicationDetailPageProps {
