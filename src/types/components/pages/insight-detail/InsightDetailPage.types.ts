51decc4e271587937acaedc0a65aa1fd7f694d25
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-12 23:25:26 +0700

    type blog detail presentation settings

--- src/types/components/pages/insight-detail/InsightDetailPage.types.ts
+++ src/types/components/pages/insight-detail/InsightDetailPage.types.ts
@@ -1,8 +1,10 @@
 import type { Insight } from "@/types/content";
+import type { CollectionEntry } from "astro:content";
 
 export interface InsightDetailPageData {
 	post: Insight;
 	relatedPosts: Insight[];
+	presentation: CollectionEntry<"blogDetailSettings">["data"];
 }
 
 export interface InsightDetailPageProps {
