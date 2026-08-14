c907e70a3d61f9b213670a1c2f194059b3615138
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-15 05:43:13 +0700

    standardize container max width

--- src/variants/components/layout/LContainer.variants.ts
+++ src/variants/components/layout/LContainer.variants.ts
@@ -2,5 +2,5 @@ import type { LContainerSize } from "@/types/components/layout/LContainer.types"
 
 export const containerSizeClasses: Record<LContainerSize, string> = {
 	site: "max-w-screen-2xl",
-	content: "max-w-7xl",
+	content: "max-w-screen-2xl",
 };
