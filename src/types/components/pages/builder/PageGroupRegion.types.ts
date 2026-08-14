796b0afbef69fadc647b7abb41c040a0d2987005
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-15 05:26:14 +0700

    refactor page group region contract

---
+++ src/types/components/pages/builder/PageGroupRegion.types.ts
@@ -0,0 +1,18 @@
+import type { CColumnsGap } from "@/types/components/object/component/CColumns.types";
+import type { PArchiveToolbarProps } from "@/types/components/object/project/archive-toolbar/PArchiveToolbar.types";
+import type { PageRegion, PageRegionBase } from "@/types/components/pages/builder/PageRegion.types";
+
+export interface PageGroupRegion extends PageRegionBase {
+	component: "group";
+	props: {
+		template: "sidebar";
+		asideLabel: string;
+		asideGap?: CColumnsGap;
+		asidePosition?: "start" | "end";
+		gap?: CColumnsGap;
+		panel?: boolean;
+		stickyAside?: boolean;
+		toolbar?: PArchiveToolbarProps;
+		regions: PageRegion[];
+	};
+}
