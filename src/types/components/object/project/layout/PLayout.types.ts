9f9106d64a73f065865ed3397056bf700669dd5f
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-15 10:55:29 +0700

    fix(layout): expose column choices

--- src/types/components/object/project/layout/PLayout.types.ts
+++ src/types/components/object/project/layout/PLayout.types.ts
@@ -1,10 +1,15 @@
 import type { LSidebarPosition } from "@/types/components/layout/LSidebar.types";
-import type { CColumnsGap } from "@/types/components/object/component/CColumns.types";
+import type {
+	CColumnsGap,
+	CColumnsTemplate,
+} from "@/types/components/object/component/CColumns.types";
 
 export interface PLayoutProps {
 	asideLabel: string;
+	asideColumns?: CColumnsTemplate;
 	asideGap?: CColumnsGap;
 	asidePosition?: LSidebarPosition;
+	columns?: CColumnsTemplate;
 	gap?: CColumnsGap;
 	panel?: boolean;
 	stickyAside?: boolean;
