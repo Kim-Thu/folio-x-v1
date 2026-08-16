5ea3ed2a6b77a779ed651570c5889dfaae331592
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 13:38:36 +0700

    feat: add video component variants

---
+++ src/variants/components/object/component/CVideo.variants.ts
@@ -0,0 +1,5 @@
+export const videoFrameClasses = "aspect-video w-full bg-black";
+
+export const videoElementClasses = "size-full object-contain";
+
+export const videoEmbedClasses = "size-full border-0";
