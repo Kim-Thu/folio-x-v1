072d1b3925045c779b4e40c09dff21f7756562bf
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 17:36:32 +0700

    Support card rail slider spacing and controls

--- src/types/components/object/component/CSlider.types.ts
+++ src/types/components/object/component/CSlider.types.ts
@@ -1,12 +1,14 @@
 import type { HTMLAttributes } from "astro/types";
 
-export type CSliderControls = "overlay" | "below" | "dots";
+export type CSliderControls = "overlay" | "below" | "below-left" | "dots";
+export type CSliderGap = "none" | "sm" | "md" | "lg" | "xl";
 export type CSliderShape = "none" | "card";
 
 export interface CSliderProps
 	extends Omit<HTMLAttributes<"div">, "class"> {
 	class?: string;
 	controls?: CSliderControls;
+	gap?: CSliderGap;
 	shape?: CSliderShape;
 	itemCount: number;
 	label: string;
