2c1fb23ceea75a3e906d622f8414f50760f261da
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 13:38:03 +0700

    feat: add dialog component variants

---
+++ src/variants/components/object/component/CDialog.variants.ts
@@ -0,0 +1,6 @@
+export const dialogClasses =
+	"w-11/12 overflow-hidden rounded-2xl bg-black p-0 text-white backdrop:bg-black/70 lg:w-3/4";
+
+export const dialogContentClasses = "relative";
+
+export const dialogCloseClasses = "absolute right-4 top-4 z-20";
