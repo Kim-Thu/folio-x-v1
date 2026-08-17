ace5c3dadfe8a77935bc4485d3448232e8fb5ac5
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 05:37:17 +0700

    style: refine product summary hierarchy

--- src/variants/components/object/project/page-header/PPageHeader.variants.ts
+++ src/variants/components/object/project/page-header/PPageHeader.variants.ts
@@ -11,19 +11,23 @@ export const pageHeaderGallerySummaryDescriptionClasses = "mt-3";
 export const pageHeaderGallerySummarySocialClasses = "mt-4";
 
 export const pageHeaderGallerySummarySocialDividerClasses =
-	"h-4 w-px shrink-0 bg-gray-100";
+	"h-4 w-px shrink-0 bg-gray-200";
 
-export const pageHeaderGallerySummaryCommerceClasses = "mt-3 flex flex-col";
+export const pageHeaderGallerySummaryCommerceClasses = "mt-7 flex flex-col";
 
-export const pageHeaderGallerySummaryPriceClasses = "mb-4";
+export const pageHeaderGallerySummaryPriceClasses = "mb-5";
 
-export const pageHeaderGallerySummaryFeaturesClasses = "flex flex-col gap-2";
+export const pageHeaderGallerySummaryFeaturesClasses = "flex flex-col gap-3";
 
-export const pageHeaderGallerySummaryActionsClasses = "mt-5";
+export const pageHeaderGallerySummaryFeatureClasses =
+	"gap-2.5 text-gray-600 [&>span:first-child]:size-5 [&>span:first-child]:rounded-full [&>span:first-child]:border-0 [&>span:first-child]:bg-blue-50 [&>span:first-child]:text-blue-600 [&>span:first-child]:ring-0";
 
-export const pageHeaderGallerySummaryActionClasses = "rounded-xl";
+export const pageHeaderGallerySummaryActionsClasses = "mt-7";
 
-export const pageHeaderGallerySummaryPaymentClasses = "mt-3";
+export const pageHeaderGallerySummaryActionClasses = "min-h-12 rounded-xl";
+
+export const pageHeaderGallerySummaryPaymentClasses =
+	"mt-4 gap-2.5 text-gray-400";
 
 export const pageHeaderGallerySummaryFactClasses =
 	"py-1 lg:border-r lg:border-gray-100 lg:px-5 lg:first:pl-0 lg:last:border-r-0 lg:last:pr-0";
