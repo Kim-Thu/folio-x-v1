3c172f61ad8ecdfaf2218256a49cd337c1430db2
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 10:12:14 +0700

    fix multi-card slider basis

--- src/variants/components/object/project/card/PCard.variants.ts
+++ src/variants/components/object/project/card/PCard.variants.ts
@@ -4,11 +4,11 @@ import type {
 } from "@/types/components/object/project/card/PCard.types";
 
 export const cardSliderItemClasses: Record<PCardColumns, string> = {
-	1: "w-full",
-	2: "w-full md:w-1/2",
-	3: "w-full md:w-1/2 lg:w-1/3",
-	4: "w-full md:w-1/2 lg:w-1/4",
-	5: "w-full md:w-1/2 lg:w-1/5",
+	1: "basis-full",
+	2: "basis-full md:basis-1/2",
+	3: "basis-full md:basis-1/2 lg:basis-1/3",
+	4: "basis-full md:basis-1/2 lg:basis-1/4",
+	5: "basis-full md:basis-1/2 lg:basis-1/5",
 };
 
 export const cardSliderItemGapClasses: Record<PCardGap, string> = {
