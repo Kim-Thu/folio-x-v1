438f398a828263eba3b601d97e8c86fce47b01ce
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 06:08:16 +0700

    fix: balance product summary columns

--- src/variants/components/object/project/page-header/PPageHeader.variants.ts
+++ src/variants/components/object/project/page-header/PPageHeader.variants.ts
@@ -1,6 +1,11 @@
 export const pageHeaderGallerySummaryRootClasses = "flex flex-col gap-6";
 
-export const pageHeaderGallerySummaryContentClasses = "flex flex-col";
+export const pageHeaderGallerySummaryGalleryClasses = "h-full";
+
+export const pageHeaderGallerySummaryContentClasses =
+	"flex h-full flex-col";
+
+export const pageHeaderGallerySummaryTopClasses = "flex flex-col";
 
 export const pageHeaderGallerySummaryIdentityClasses = "flex flex-col";
 
@@ -22,7 +27,11 @@ export const pageHeaderGallerySummaryFeaturesClasses = "flex flex-col gap-3";
 export const pageHeaderGallerySummaryFeatureClasses =
 	"gap-2.5 text-gray-600 [&>span:first-child]:size-5 [&>span:first-child]:rounded-full [&>span:first-child]:border-0 [&>span:first-child]:bg-blue-50 [&>span:first-child]:text-blue-600 [&>span:first-child]:ring-0";
 
-export const pageHeaderGallerySummaryActionsClasses = "mt-7";
+export const pageHeaderGallerySummaryFooterClasses =
+	"mt-auto pt-7";
+
+export const pageHeaderGallerySummaryActionsClasses =
+	"items-center";
 
 export const pageHeaderGallerySummaryActionClasses = "min-h-12 rounded-xl";
 
