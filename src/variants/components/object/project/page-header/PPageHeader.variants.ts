0f677ce28d0fea7ed14d79361bfd5f76219c7ca1
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 02:57:48 +0700

    refactor: group product summary hierarchy

--- src/variants/components/object/project/page-header/PPageHeader.variants.ts
+++ src/variants/components/object/project/page-header/PPageHeader.variants.ts
@@ -1,21 +1,25 @@
-export const pageHeaderGallerySummaryRootClasses = "flex flex-col gap-8";
+export const pageHeaderGallerySummaryRootClasses = "flex flex-col gap-6";
 
 export const pageHeaderGallerySummaryContentClasses = "flex flex-col";
 
-export const pageHeaderGallerySummaryTitleClasses = "mt-2";
+export const pageHeaderGallerySummaryIdentityClasses = "flex flex-col gap-3";
 
-export const pageHeaderGallerySummaryDescriptionClasses = "mt-3";
+export const pageHeaderGallerySummaryTitleRowClasses = "mt-1";
 
-export const pageHeaderGallerySummaryRatingClasses = "mt-4";
+export const pageHeaderGallerySummarySocialClasses = "mt-4";
 
-export const pageHeaderGallerySummaryPriceClasses = "mt-3";
+export const pageHeaderGallerySummarySocialDividerClasses =
+	"h-4 w-px shrink-0 bg-gray-100";
 
-export const pageHeaderGallerySummaryFeaturesClasses =
-	"mt-5 flex flex-col gap-2";
+export const pageHeaderGallerySummaryCommerceClasses = "mt-4 flex flex-col";
 
-export const pageHeaderGallerySummaryActionsClasses = "mt-6";
+export const pageHeaderGallerySummaryPriceClasses = "mb-5";
 
-export const pageHeaderGallerySummaryPaymentClasses = "mt-4";
+export const pageHeaderGallerySummaryFeaturesClasses = "flex flex-col gap-2";
+
+export const pageHeaderGallerySummaryActionsClasses = "mt-5";
+
+export const pageHeaderGallerySummaryPaymentClasses = "mt-3";
 
 export const pageHeaderGallerySummaryFactClasses =
 	"py-1 lg:border-r lg:border-gray-100 lg:px-5 lg:first:pl-0 lg:last:border-r-0 lg:last:pr-0";
