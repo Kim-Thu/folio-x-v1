65e349048c79dbc836da4669239acb9daf16b42f
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 03:09:07 +0700

    chore: keep existing page header contract

--- src/types/components/object/project/page-header/PPageHeader.types.ts
+++ src/types/components/object/project/page-header/PPageHeader.types.ts
@@ -147,7 +147,6 @@ export interface PPageHeaderGallerySummaryData {
     value: number;
     maximum: number;
     count: number;
-    reviewsLabel: string;
     salesLabel: string;
   };
   price: {
