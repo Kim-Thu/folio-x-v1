eaac139cfad75606d65821e499d3ed256cea7108
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 13:09:33 +0700

    fix: make embla island behavior only

--- src/types/components/object/component/CSliderEmbla.types.ts
+++ src/types/components/object/component/CSliderEmbla.types.ts
@@ -1,10 +1,3 @@
-import type { ReactNode } from "react";
-
 import type { CSliderBehavior } from "@/types/components/object/component/CSlider.types";
 
-export interface CSliderEmblaProps extends Required<CSliderBehavior> {
-	children: ReactNode;
-	containerClassName: string;
-	label: string;
-	viewportClassName: string;
-}
+export type CSliderEmblaProps = Required<CSliderBehavior>;
