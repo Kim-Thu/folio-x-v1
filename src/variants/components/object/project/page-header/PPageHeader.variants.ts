281b19a0aec561c9374f5d84863a3f6f4c8d4aec
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 02:50:04 +0700

    style: define gallery summary hierarchy

---
+++ src/variants/components/object/project/page-header/PPageHeader.variants.ts
@@ -0,0 +1,21 @@
+export const pageHeaderGallerySummaryRootClasses = "flex flex-col gap-8";
+
+export const pageHeaderGallerySummaryContentClasses = "flex flex-col";
+
+export const pageHeaderGallerySummaryTitleClasses = "mt-2";
+
+export const pageHeaderGallerySummaryDescriptionClasses = "mt-3";
+
+export const pageHeaderGallerySummaryRatingClasses = "mt-4";
+
+export const pageHeaderGallerySummaryPriceClasses = "mt-3";
+
+export const pageHeaderGallerySummaryFeaturesClasses =
+	"mt-5 flex flex-col gap-2";
+
+export const pageHeaderGallerySummaryActionsClasses = "mt-6";
+
+export const pageHeaderGallerySummaryPaymentClasses = "mt-4";
+
+export const pageHeaderGallerySummaryFactClasses =
+	"py-1 lg:border-r lg:border-gray-100 lg:px-5 lg:first:pl-0 lg:last:border-r-0 lg:last:pr-0";
