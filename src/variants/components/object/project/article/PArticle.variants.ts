1288fa9fe3c001f4fdb1e58de1af3bc88a3d3776
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 07:06:28 +0700

    refactor: add article document styles

---
+++ src/variants/components/object/project/article/PArticle.variants.ts
@@ -0,0 +1,6 @@
+export const articleDocumentClasses = [
+	"min-w-0",
+	"[&>h2]:mt-8 [&>h2]:scroll-mt-24 [&>h2]:font-semibold [&>h2]:text-2xl [&>h2]:leading-snug [&>h2]:first:mt-0 [&>h2]:md:mt-10 [&>h2]:md:text-3xl",
+	"[&>h3]:mt-8 [&>h3]:scroll-mt-24 [&>h3]:font-semibold [&>h3]:text-xl [&>h3]:leading-snug [&>h3]:first:mt-0 [&>h3]:md:mt-10",
+	"[&>p]:mt-4 [&>p]:text-base [&>p]:text-gray-600 [&>p]:first:mt-0",
+].join(" ");
