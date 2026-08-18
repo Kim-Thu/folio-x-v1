ac17f511f8718bdefcd846b2d4ec63d24619181a
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 16:50:29 +0700

    Fix newsletter advertisement media as bottom background layer

--- src/variants/components/object/project/advertisement/PAdvertisement.variants.ts
+++ src/variants/components/object/project/advertisement/PAdvertisement.variants.ts
@@ -4,6 +4,4 @@ export const advertisementFormFirstContentClasses =
 	"relative z-10 h-1/2";
 
 export const advertisementFormFirstMediaClasses =
-	"absolute inset-x-0 bottom-0 flex h-1/2 items-end justify-center";
-
-export const advertisementFormFirstImageClasses = "w-3/4";
+	"top-auto h-1/2";
