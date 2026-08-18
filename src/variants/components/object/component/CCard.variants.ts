a7d547dc226c80997f7c845df352819d02ffb6ad
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 08:39:40 +0700

    fix: add product card stretch variants

---
+++ src/variants/components/object/component/CCard.variants.ts
@@ -0,0 +1,7 @@
+export const cardMediaDetailsRootClasses = "flex h-full flex-col";
+
+export const cardMediaDetailsBodyClasses = "flex flex-1";
+
+export const cardMediaDetailsContentClasses = "h-full flex-1";
+
+export const cardMediaDetailsFooterClasses = "mt-auto";
