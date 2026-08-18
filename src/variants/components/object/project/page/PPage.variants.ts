4f2f7f0f1f6197b1e2a48bd4c01b37fad979cfbd
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 09:10:54 +0700

    feat: add mobile faceted archive presentation

---
+++ src/variants/components/object/project/page/PPage.variants.ts
@@ -0,0 +1,12 @@
+export const pageFacetedDesktopSidebarClasses = "hidden lg:block";
+
+export const pageFacetedMobileFilterClasses =
+	"rounded-2xl ring-1 ring-inset ring-gray-100 lg:hidden";
+
+export const pageFacetedMobileFilterSummaryClasses =
+	"flex cursor-pointer list-none items-center justify-between px-4 py-3 text-sm font-medium text-black marker:hidden [&::-webkit-details-marker]:hidden";
+
+export const pageFacetedMobileFilterContentClasses =
+	"border-t border-gray-100 p-4";
+
+export const pageFacetedMobileAdvertisementClasses = "lg:hidden";
