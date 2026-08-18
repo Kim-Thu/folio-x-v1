d469411eb765d7e8ac391b58fa1adb8bb8d996d2
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 13:04:15 +0700

    Style rich article content

--- src/variants/components/object/project/article/PArticle.variants.ts
+++ src/variants/components/object/project/article/PArticle.variants.ts
@@ -3,4 +3,12 @@ export const articleDocumentClasses = [
 	"[&>h2]:mt-8 [&>h2]:scroll-mt-24 [&>h2]:font-semibold [&>h2]:text-2xl [&>h2]:leading-snug [&>h2]:first:mt-0 [&>h2]:md:mt-10 [&>h2]:md:text-3xl",
 	"[&>h3]:mt-8 [&>h3]:scroll-mt-24 [&>h3]:font-semibold [&>h3]:text-xl [&>h3]:leading-snug [&>h3]:first:mt-0 [&>h3]:md:mt-10",
 	"[&>p]:mt-4 [&>p]:text-base [&>p]:text-gray-600 [&>p]:first:mt-0",
+	"[&_strong]:font-semibold [&_b]:font-semibold [&_em]:italic [&_i]:italic [&_del]:line-through [&_s]:line-through",
+	"[&_a]:font-medium [&_a]:text-blue-600 [&_a]:underline [&_a]:underline-offset-4",
+	"[&_code]:rounded-md [&_code]:bg-gray-100 [&_code]:px-1 [&_code]:py-0.5 [&_code]:font-mono [&_code]:text-sm",
+	"[&>figure]:mt-6 [&>figure]:first:mt-0 [&>figure]:md:mt-8 [&>figure_img]:w-full [&>figure_img]:rounded-2xl [&>figure_figcaption]:mt-2 [&>figure_figcaption]:text-sm [&>figure_figcaption]:leading-relaxed [&>figure_figcaption]:text-gray-500",
+	"[&>img]:mt-6 [&>img]:w-full [&>img]:rounded-2xl [&>img]:first:mt-0 [&>img]:md:mt-8",
+	"[&>ul]:mt-4 [&>ul]:list-disc [&>ul]:space-y-2 [&>ul]:pl-6 [&>ul]:text-gray-600",
+	"[&>ol]:mt-4 [&>ol]:list-decimal [&>ol]:space-y-2 [&>ol]:pl-6 [&>ol]:text-gray-600",
+	"[&>blockquote]:mt-6 [&>blockquote]:border-l-2 [&>blockquote]:pl-5 [&>blockquote]:text-gray-600 [&>blockquote]:md:mt-8",
 ].join(" ");
