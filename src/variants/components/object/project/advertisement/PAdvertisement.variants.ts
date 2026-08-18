d44b1ad5e6aa823aee088264b6e6455bf999940c
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 16:41:59 +0700

    style(advertisement): match newsletter card proportions

---
+++ src/variants/components/object/project/advertisement/PAdvertisement.variants.ts
@@ -0,0 +1,9 @@
+export const advertisementFormFirstRootClasses = "relative aspect-[8/9]";
+
+export const advertisementFormFirstContentClasses =
+	"relative z-10 h-1/2";
+
+export const advertisementFormFirstMediaClasses =
+	"absolute inset-x-0 bottom-0 flex h-1/2 items-end justify-center";
+
+export const advertisementFormFirstImageClasses = "w-3/4";
