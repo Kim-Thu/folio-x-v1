fcfe39b143b6eca72ce091aac9382a1093269c98
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 08:34:00 +0700

    style: align pricing CTA check icons with product detail

---
+++ src/variants/components/object/project/cta/PCta.variants.ts
@@ -0,0 +1,2 @@
+export const ctaMediaPricingFeatureClasses =
+	"gap-2.5 text-gray-600 [&>span:first-child]:size-5 [&>span:first-child]:rounded-full [&>span:first-child]:border-0 [&>span:first-child]:bg-blue-50 [&>span:first-child]:text-blue-600 [&>span:first-child]:ring-0";
