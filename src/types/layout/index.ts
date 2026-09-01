62f4afddc8226d0dc3b196aa446f11cb0c2af1f5
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-09-01 17:39:56 +0700

    refactor(types): remove obsolete layout barrel (#27)

--- src/types/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export type { BaseLayoutProps } from '@/types/layout/BaseLayoutProps';
-export type { HeaderProps } from '@/types/layout/HeaderProps';
-export type { HeaderContactProps, HeaderContactVariant } from '@/types/layout/HeaderContactProps';
