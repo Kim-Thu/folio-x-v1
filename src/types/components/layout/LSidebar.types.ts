c77ab42b62f7368df1977ff0c3140a07870b272e
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-09-03 00:07:35 +0700

    fix: support responsive sidebar ordering

--- src/types/components/layout/LSidebar.types.ts
+++ src/types/components/layout/LSidebar.types.ts
@@ -5,6 +5,7 @@ export type LSidebarPosition = "start" | "end";
 export interface LSidebarProps extends Omit<HTMLAttributes<"div">, "class"> {
 	class?: string;
 	label: string;
+	mobilePosition?: LSidebarPosition;
 	position?: LSidebarPosition;
 	sticky?: boolean;
 }
