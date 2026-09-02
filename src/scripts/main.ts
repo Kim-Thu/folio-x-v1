664c9373178cece6d883e700cf6758b54082c914
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-09-02 19:59:35 +0700

    feat: initialize scroll to top

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,9 +3,11 @@ import { initArchiveFilter } from './archive-filter';
 import { initHeader } from './header';
 import { initReveal } from './reveal';
 import { initScrollProgress } from './scroll-progress';
+import { initScrollToTop } from './scroll-to-top';
 
 initHeader();
 initScrollProgress();
+initScrollToTop();
 initAccordion();
 initArchiveFilter();
 initReveal();
